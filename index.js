cd8e1e2433cf6cc504682e6445ab2d73f73ec535
benardcheruiyot <bcheruiyot221@gmail.com>
2022-08-28 13:23:47 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+const main = document.getElementById('victory');
+main.remove();
+
+
+//Create new heading element with ID atttribute
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute('id', 'victory');
+
+newHeader.textContent = 'Gilbert-Koech is the champion';
+
+main.appendChild(newHeader);
